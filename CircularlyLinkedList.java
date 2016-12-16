dc61c7a98d4fbb320baf709f72baffd174067d51
amanshankar <amanshankar9393@gmail.com>
2016-12-16 01:06:27 -0500

    Test
    Test

---
+++ CircularlyLinkedList.java
@@ -0,0 +1,16 @@
+
+public class CircularlyLinkedList {
+	CircularlyLinkedList tail = null;
+	private int size; 
+	public CircularlyLinkedList(){};
+	public boolean isEmpty(){return size ==0;}
+	public CircularlyLinkedList first(){
+		if(isEmpty()) return null;
+		//return tail.getNext().getElement();
+		
+	}
+
+	
+			
+
+}
